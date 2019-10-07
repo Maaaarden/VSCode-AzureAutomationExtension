e61631f803d56e52299bf2c402338f296d319abe
Wolter Kamphuis <wolter@wolterkamphuis.nl>
2019-10-07 13:11:42 +0200

    Replace hardcoded directory seperator \ to path.sep

--- modules/LocalAutomation.js
+++ modules/LocalAutomation.js
@@ -1,18 +1,20 @@
 var openLocalRunbook = function (runbookName) {
     var vscode = require('vscode')
+    var path = require('path')
 
-    var path = vscode.workspace.rootPath + `\\${runbookName}.ps1`
-    vscode.workspace.openTextDocument(path).then(doc => {
+    var rootPath = vscode.workspace.rootPath + path.sep + `${runbookName}.ps1`
+    vscode.workspace.openTextDocument(rootPath).then(doc => {
         vscode.window.showTextDocument(doc)
     })
 }
 
 var checkIfLocalExist = function (runbookName, next) {
     var vscode = require('vscode')
+    var path = require('path')
 
-    var path = vscode.workspace.rootPath + `\\${runbookName}.ps1`
+    var rootPath = vscode.workspace.rootPath + path.sep + `${runbookName}.ps1`
 
-    if(path) {
+    if(rootPath) {
         next(true)
     } else {
         next(false)
